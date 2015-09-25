a0657063c76ac6f03b1624f55bfe368c9578999a
alireza-saberi <saberi.ar.eng@gmail.com>
2015-09-25 18:46:49 -0400

    chore: updated gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -398,7 +398,7 @@ module.exports = function (grunt) {
     },
     heroku: {
       options: {
-        remote: 'git@heroku.com:example-heroku-webapp-1988.git',
+        remote: 'git@heroku.com:safe-oasis-4899.git',
         branch: 'master',
         tag: pkg.version
       }
